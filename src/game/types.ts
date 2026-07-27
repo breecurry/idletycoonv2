d4202f1326119733a374171f803a2a9d3c96c952
Breanna Curry <breannacurry@Breannas-MBP.home.local>
2026-07-26 22:07:18 -0400

    added business type to types.ts file

--- src/game/types.ts
+++ src/game/types.ts
@@ -1,12 +1,16 @@
 export type GameState = {
-    money: number;
-    managers: number;
-    training: number;
-    tapPower: number;
-    lifetimeRun: number;
-    lifetimeTotal: number;
-    bestMoney: number;
-    brandValue: number;
-    rebrands: number;
-    owned: { [id: string]: number };
+  money: number;
+  managers: number;
+  training: number;
+  tapPower: number;
+  lifetimeRun: number;
+  lifetimeTotal: number;
+  bestMoney: number;
+  brandValue: number;
+  rebrands: number;
+  owned: { [id: string]: number };
+};
+
+export type Business = {
+  minRebrands?: number;
 };
