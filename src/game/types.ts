e248d2a9ff2fdf459756e67719b5fefe63791c82
Breanna Curry <breannacurry@Breannas-MBP.home.local>
2026-07-26 22:01:07 -0400

    added types.ts folder with money down to owned new learning style on owned

---
+++ src/game/types.ts
@@ -0,0 +1,12 @@
+export type GameState = {
+    money: number;
+    managers: number;
+    training: number;
+    tapPower: number;
+    lifetimeRun: number;
+    lifetimeTotal: number;
+    bestMoney: number;
+    brandValue: number;
+    rebrands: number;
+    owned: { [id: string]: number };
+};
